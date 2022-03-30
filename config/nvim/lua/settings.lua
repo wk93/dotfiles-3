f5ea08087be939fdc0e5f825c4bf7677987d6380
Wojciech Kania <wojciech.kania@me.com>
2022-03-30 13:44:27 +0200

    nvim: text display settings

--- config/nvim/lua/settings.lua
+++ config/nvim/lua/settings.lua
@@ -1,2 +1,25 @@
-vim.opt.number = true
-vim.opt.relativenumber = true
+local opt = vim.opt
+
+-- tab size
+opt.expandtab = true
+opt.shiftwidth = 2
+opt.tabstop = 2
+opt.softtabstop = 2
+
+-- line numbers
+opt.number = true
+opt.relativenumber = true
+
+-- line wrapping
+opt.wrap = false
+opt.linebreak = true
+
+-- hidden chars
+opt.list = true
+opt.listchars = {
+  nbsp = '⦸', -- non-breaking space
+  tab = '  ', -- change tab to space
+  trail = '·', -- trailing spaces
+  extends = '»', -- more text on right side
+  precedes = '«', -- more text on left side
+}
